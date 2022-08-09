c43413c4bc194d407534d8bca9c9b67bfb4aa404
Pan Liu <us.panliu@gmail.com>
2022-08-09 15:58:15 -0700

    handle video press to pause or play, initialize footer and sidebar

---
+++ client/src/components/VideoFooter.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './VideoFooter.css';
+
+function VideoFooter() {
+  return (
+    <div className="video_footer">
+    </div>
+  );
+}
+
+export default VideoFooter;
+
---
+++ client/src/components/VideoSidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './VideoSidebar.css';
+
+function VideoSidebar() {
+  return (
+    <div className="video_sidebar">
+    </div>
+  );
+}
+
+export default VideoSidebar;
