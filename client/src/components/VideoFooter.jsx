0d064d7a24690d31af3750e7a4154005f6148402
Pan Liu <us.panliu@gmail.com>
2022-08-10 00:20:20 -0700

    add ticker and record to the video footer, change font

--- client/src/components/VideoFooter.jsx
+++ client/src/components/VideoFooter.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './VideoFooter.css';
 import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import Ticker from 'react-ticker';
+import record from '../assets/music_record.png'
 
 function VideoFooter() {
   return (
@@ -10,8 +12,23 @@ function VideoFooter() {
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         <div className="video_footer_music">
           <MusicNoteIcon className="video_footer_music_note" />
+          <Ticker mode="smooth">
+            {({ index }) => (
+                <>
+                    <p>This is a song!</p>
+                </>
+            )}
+          </Ticker>
         </div>
       </div>
+      {/* <img
+        className="video_footer_record"
+        src="https://pixy.org/src2/689/6893569.png" alt="spinning music record"
+      /> */}
+      <img
+        className="video_footer_record"
+        src={record} alt="spinning music record"
+      />
     </div>
   );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,10 @@ module.exports = {
          test: /\.(css)$/,
          use: ['style-loader','css-loader']
       },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: ['file-loader']
+      }
     ]
   },
   resolve: {
