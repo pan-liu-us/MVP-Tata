582e1288c04bf4728e8bc6ce4a6f57e85637b3a7
Pan Liu <us.panliu@gmail.com>
2022-08-09 17:09:01 -0700

    add username, description and music note icon to video footer

--- client/src/components/VideoFooter.jsx
+++ client/src/components/VideoFooter.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import './VideoFooter.css';
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
 
 function VideoFooter() {
   return (
     <div className="video_footer">
+      <div className="video_footer_text">
+        <h3>@bellapoarch</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <div className="video_footer_music">
+          <MusicNoteIcon className="video_footer_music_note" />
+        </div>
+      </div>
     </div>
   );
 }
